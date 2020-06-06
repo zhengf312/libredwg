78e2f90ea6d132cefef59f9ede8a7f3fcc2c880b
Reini Urban <rurban@cpan.org>
2020-06-06 09:58:01 +0200

    add relink.sh script
    to symlink files to the gambas srcdir

---
+++ bindings/gambas/relink.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+# need to know the gambas srcdir
+test -z "$GB_SRCDIR" && GB_SRCDIR="$1"
+thisdir=$(realpath "$0")
+thisdir=$(dirname "$thisdir")
+
+find_gbsrc () {
+	dir="$1"
+	if [ -d "$dir" ] && [ -d "$dir/main/share" ]
+	then
+		GB_SRCDIR=$(realpath "$dir")
+	fi
+}
+test -z "$GB_SRCDIR" && find_gbsrc ../gambas
+test -z "$GB_SRCDIR" && find_gbsrc ../../../gambas
+test -z "$GB_SRCDIR" && find_gbsrc /usr/src/gambas
+
+cd "$thisdir" || exit
+for f in component.am reconf m4 COPYING
+do
+	if [ ! -e $f ]
+	then
+		ln -s "$GB_SRCDIR"/$f $f
+	fi
+done
+for f in gambas.h gb_common.h
+do
+	if [ ! -e $f ]
+	then
+		ln -s "$GB_SRCDIR"/main/share/$f $f
+	fi
+done
+cd - || exit
